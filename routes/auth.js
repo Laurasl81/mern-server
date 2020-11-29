d7368cf9768733976b49c8e98248743a0b056040
Laura Sanchez <laura695hotmail.com>
2020-11-29 17:20:24 +0100

    Initial commit

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+// rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const {check} = require('express-validator');
+const authController = require ('../controllers/authController.js')
+const auth = require('../middleware/auth')
+
+
+// Iniciar sesión
+//api/auth
+router.post('/',
+authController.autenticarUsuario
+);
+
+// Obtiene el usuario autenticado
+router.get('/',
+auth,
+authController.usuarioAutenticado
+)
+module.exports = router;

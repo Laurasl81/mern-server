75900ad7a8e6bc1738e93ca159550cb064deab9c
Laura Sanchez <laura695hotmail.com>
2020-12-07 19:32:08 +0100

    ajustes

--- index.js
+++ index.js
@@ -23,13 +23,6 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
-
-
-// Definir la pagina principal
-app.get('/', (req, res) => {
-    res.send('Hola Mundo')
-});
-
 // arrancar la app
 app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor esta funcionado en el puerto ${port}`);
